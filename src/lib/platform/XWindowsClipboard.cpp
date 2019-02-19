ccc4cea026bb911b9711b77153133e59fbfcce38
Kuang Rufan <kuangrufan@iauto.com>
2019-02-19 16:28:38 +0800

    BUGFIX: fix garbled string when pasting strings from/to Linux.

--- src/lib/platform/XWindowsClipboard.cpp
+++ src/lib/platform/XWindowsClipboard.cpp
@@ -2,11 +2,11 @@
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2012-2016 Symless Ltd.
  * Copyright (C) 2002 Chris Schoeneman
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file LICENSE that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -407,8 +407,8 @@ XWindowsClipboard::getConverter(Atom target, bool onlyIfNotAdded) const
 	IXWindowsClipboardConverter* converter = NULL;
 	for (ConverterList::const_iterator index = m_converters.begin();
 								index != m_converters.end(); ++index) {
-		converter = *index;
-		if (converter->getAtom() == target) {
+		if ((*index)->getAtom() == target) {
+			converter = *index;
 			break;
 		}
 	}
