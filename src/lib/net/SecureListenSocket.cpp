78ea15bbd2663741700095b9c0ee7da71d144726
Kuang Rufan <kuangrufan@iauto.com>
2019-02-19 13:49:38 +0800

    BUGFIX: fix double delete.
    Fix double delete when quiting synergy server if there're clients
    connected.

--- src/lib/net/SecureListenSocket.cpp
+++ src/lib/net/SecureListenSocket.cpp
@@ -1,11 +1,11 @@
 /*
  * synergy -- mouse and keyboard sharing utility
  * Copyright (C) 2015-2016 Symless Ltd.
- * 
+ *
  * This package is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * found in the file LICENSE that should have accompanied this file.
- * 
+ *
  * This package is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -39,11 +39,6 @@ SecureListenSocket::SecureListenSocket(
 
 SecureListenSocket::~SecureListenSocket()
 {
-	SecureSocketSet::iterator it;
-	for (it = m_secureSocketSet.begin(); it != m_secureSocketSet.end(); it++) {
-		delete *it;
-	}
-	m_secureSocketSet.clear();
 }
 
 IDataSocket*
@@ -73,8 +68,6 @@ SecureListenSocket::accept()
 		}
 
 		socket->secureAccept();
-
-		m_secureSocketSet.insert(socket);
 
 		return dynamic_cast<IDataSocket*>(socket);
 	}
